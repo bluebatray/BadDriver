d6be9b0ef01f68fceadf865546942eb010c41ec9
Jake Blumer <bluebatray@gmail.com>
2025-06-23 00:10:55 +0200

    lcd added along with notes

--- BadDriver.ino
+++ BadDriver.ino
@@ -7,12 +7,27 @@ Libraries (build-in)
 SPI
 SD
 
+Pin   Device  Pin   Wire Color
+---   ------  ---   ----------  
+5V    Bread         Red
+GND   Bread         Black
+
+A4    Accel   SDA   Blue
+A5    Accel   SCL   Yellow
+
+D4    SD Card CS    Yellow
+D11   SD CARD MOSI  Blue
+D12   SD CARD MISO  Green
+D13   SD CARD SCK   Orange
+
+
 -------------------------------
 */
 
 //Libraries
 #include <SPI.h>
 #include <SD.h>
+#include <LiquidCrystal.h>
 
 #include "ADXL345_Wrapper.hpp"
 #include "vec3.hpp"
@@ -23,6 +38,7 @@ ADXL345_Wrapper accelerometer = ADXL345_Wrapper();
 // 1 g = 9.8 m/s^2
 
 const int chipSelect = 10;
+LiquidCrystal lcd(8, 9, 5, 6, 7, 10);
 
 void setup(void) 
 { 
@@ -30,6 +46,9 @@ void setup(void)
   Serial.begin(9600); 
 
   while(!Serial);
+
+  lcd.begin(16, 2);
+  lcd.print("Hello driver!");
 
   if (!SD.begin(chipSelect)) {
     Serial.println("initialization failed. Things to check:");
@@ -62,6 +81,9 @@ void loop(void)
     String(sensorValues.x, 2) + "," + 
     String(sensorValues.y, 2) + "," + 
     String(sensorValues.z, 2);
+
+    lcd.setCursor(0, 1);
+    lcd.print(dataString);
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
